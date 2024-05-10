646efd2bff82ab592d699b431c8f6e2366c06f27
mrendyg <andyg.oyarzo@gmail.com>
2024-05-10 13:41:10 -0400

    diesño de tabla

--- frontend/src/app/user-list/user-list.component.ts
+++ frontend/src/app/user-list/user-list.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
 })
 export default class UserListComponent implements OnInit{
   
   private userService = inject(UserService)
 
+  users: any[]=[];
+
   ngOnInit(): void {
     this.userService.list()
-      .subscribe(users => {
-        console.log(users)
+      .subscribe((users: any) => {
+        this.users = users;
       })
   }
 }
